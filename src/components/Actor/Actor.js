a57d53b9c7f0e9ddbbcc5d8193fdeba22f730dd5
A C <72420910+acphil2@users.noreply.github.com>
2021-03-15 18:52:35 -0700

    added movement

--- src/components/Actor/Actor.js
+++ src/components/Actor/Actor.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Sprite from '../Sprite/Sprite';
 
-const Actor = ({ data, step = 0, dir = 0 }) => {
+const Actor = ({ 
+    data, 
+    position = { x: 0, y: 0 },
+    step = 0, 
+    dir = 0
+    }) => {
+    
 	const{ h, w } = data; 
 	return (
 		<Sprite 
             image={'Sprites/spritesheet.png'}
+            position={position}
             data={{
                 x: step * w,
                 y: dir * h,
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import Actor from '../Actor/Actor';
 import useKeyPress from './../../hooks/UseKeyPress/use-key-press.js';
+import useWalk from './../../hooks/UseWalk/use-walk.js';
+
 
 const Player = () => {
+	const { dir, step, walk, position } = useWalk(2);
 	const data = {
     	h: 50,
         w: 25
     };
 
     useKeyPress((e) => {
-    	const dir = e.key.replace('Arrow', '').toLowerCase()
-    	console.dir(dir);
+    	walk(e.key.replace('Arrow', '').toLowerCase());
+
     	e.preventDefault();
     });
 
-    return <Actor data={data}/>
-	
+    return( 
+    	<Actor 
+    		data={data} 
+    		step={step} 
+    		dir={dir}
+    		position={position}
+    	/>
+    );
 }
 
 export default Player;
