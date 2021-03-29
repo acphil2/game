c7980806eeea5385632f474645c4775e2fbdcf82
A C <72420910+acphil2@users.noreply.github.com>
2021-03-29 12:05:19 -0700

    candle components set up

--- src/App.js
+++ src/App.js
@@ -3,12 +3,37 @@ import Player from './components/Player/Player';
 import Candle from './components/Candle/Candle';
 import './App.css';
 
-export default function App() {
+export default function App(props) {
    return (
       	<div className="game">
       		{/* directions, button */}
             <Player/>
-            <Candle/>
+            <Candle style= {{
+            	top: '25%',
+            	left: '40%'
+            }}
+            />
+            <Candle style= {{
+            	top: '50%',
+            	left: '82%'
+            }}/>
+            <Candle style= {{
+            	top: '50%',
+            	left: '20%'
+            }}/>
+            <Candle style= {{
+            	top: '75%',
+            	left: '40%'
+            }}/>
+            <Candle style= {{
+            	top: '100%',
+            	left: '50%'
+            }}/>
+            <Candle style= {{
+            	top: '100%',
+            	left: '78%'
+            }}
+            />
       	</div>
    	);
 }
--- src/components/Candle/Candle.js
+++ src/components/Candle/Candle.js
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react';
 import './Candle.css';
 
-const Candle = () => {
+const Candle = ( props ) => {
     const [toggleClass, setToggleClass] = useState(true);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setToggleClass(toggleClass => toggleClass===false);
-    }, 500);
+        }, 500);
         return () => clearInterval(interval);
-    }, []);
+    }, [toggleClass]);
 
     return(
-	<div className={`candle-sprite ${toggleClass ? 'candle-flicker' : null}`} 
-	>
-
-	</div>
+    	<div className={`candle-sprite ${toggleClass ? 'candle-flicker' : null}`}
+        style={props.style}
+        >
+    	</div>
     );
 }
 
