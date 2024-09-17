c113e376188cd9fcede0a3d9453c43495c942d46
A C <72420910+acphil2@users.noreply.github.com>
2024-09-17 15:51:17 -0700

    responsive font, padding issue

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -6,7 +6,7 @@ const Start = ( props ) => {
         <div className='start'>
         	<div className='title'>Holly<br/><span className='title-size'>Haunted</span></div>
         	<div className='start-btn'>LOADING...</div>	
-        	<div className='instructions'> Use spacebar to put candles out before they relight! <div className='outline'></div><br/> <br/> Use keyboard arrows to move around.</div>
+        	<div className='instructions'> Use spacebar to put candles out before they relight! <div className='outline'></div><br/> Use keyboard arrows to move.</div>
         </div>
     );
 }
