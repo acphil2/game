34fd2302032922bdb8218880ec6148ce6d8c3a52
A C <72420910+acphil2@users.noreply.github.com>
2024-09-17 12:13:44 -0700

    update text, small changes

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -6,7 +6,7 @@ const Start = ( props ) => {
         <div className='start'>
         	<div className='title'>Holly<br/><span className='title-size'>Haunted</span></div>
         	<div className='start-btn'>LOADING...</div>	
-        	<div className='instructions'> Put out all of the candles! <div className='outline'></div><br/> <br/> Use spacebar and keyboard arrows or the buttons below.</div>
+        	<div className='instructions'> Use spacebar to put candles out before they relight! <div className='outline'></div><br/> <br/> Use keyboard arrows to move around.</div>
         </div>
     );
 }
